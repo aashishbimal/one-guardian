a31135146c4cb7b24a0e4580a8c00f042fbe443e
Sahil Satija <sahilsatija2014@gmail.com>
2020-05-19 00:36:04 +0530

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import NavBar from '../src/components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import Skills from './components/Skills/Skills';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     <NavBar />
+     <Route path="/" component={NavBar} />
+     <Route path="/home" component={Home} />
+     <Route path="/skills" component={Skills} />
     </div>
+    </Router>
   );
 }
 
