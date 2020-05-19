fd3c94a728bacfe6d2c9d5085366228a1053af1c
Sahil Satija <sahilsatija2014@gmail.com>
2020-05-19 12:00:41 +0530

    project list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import NavBar from '../src/components/NavBar/NavBar';
-import Home from './components/Home/Home';
+import Card from './components/Cards/Card';
 import Skills from './components/Skills/Skills';
+import Layout from './components/Layout/Layout';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
     <div className="App">
-     <Route path="/" component={NavBar} />
-     <Route path="/home" component={Home} />
+     <Route path="/" component={Layout} />
+     <Route path="/home" component={Card} />
      <Route path="/skills" component={Skills} />
     </div>
     </Router>
